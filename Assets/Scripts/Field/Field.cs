3b9aeab72e19b367c1c60428f18ed5ba17e1cabc
AsaeNeopie <n.noahchery@gmail.com>
2024-03-27 10:41:44 +0100

    On peut planter !!!!

--- Assets/Scripts/Field/Field.cs
+++ Assets/Scripts/Field/Field.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Field : MonoBehaviour
 {
     public GameObject Carrot;
-    
+    [HideInInspector] public int _maxCarrot = 1;
 
    public void TryToPlantAt(Vector3 point)
     {
-        //regarder si le champ est pas plein (connard)
-        // add 1 a carrot
-        Instantiate(Carrot, point, Quaternion.identity);
+        Debug.Log("ScriptLancé");
+        if (_maxCarrot <= 5)         
+        {
+            Instantiate(Carrot, point, Quaternion.identity);
+            _maxCarrot++;
+        }         
     }
 }
--- Assets/Scripts/Player/PlayerInteract.cs
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,7 @@ public class PlayerInteract : MonoBehaviour
     [SerializeField] float _maxRaycastHit;
     [SerializeField] Plant _plantScript;
     [SerializeField] PlayerInventory _inventory;
+    [SerializeField] Camera _cam;
     public void OnInteract(InputAction.CallbackContext context)
     {
         if (context.canceled) return;
@@ -14,29 +16,32 @@ public class PlayerInteract : MonoBehaviour
 
     private void Interaction()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = new Ray(transform.position, _cam.transform.forward);
         RaycastHit hitData;
+
         if(Physics.Raycast(ray, out hitData, _maxRaycastHit))
         {
-        Debug.Log (hitData.collider);
-        Debug.DrawRay(ray.origin, hitData.point - transform.position, Color.red, 10f);
-        if (hitData.collider)
-        {
+            Debug.Log (hitData.collider);
+            Debug.DrawRay(ray.origin, hitData.point - transform.position, Color.red, 10f);
+
+            if (hitData.collider == null) return;
+
             Plant plant = hitData.collider.GetComponent<Plant>();
-            if (plant == null || !plant._canHarvest) return;
-            Destroy(hitData.collider.gameObject);
-            _inventory.carrot ++;
-        }
-        if (hitData.collider)
-        {
-            if(hitData.collider.TryGetComponent<Field>(out Field field))
+            if (plant != null && plant._canHarvest)
             {
-                // voir si il y a assez de graine
+                Destroy(hitData.collider.gameObject);
+                _inventory.carrot++;
+                return;
+            }
+
+            Field field = hitData.collider.GetComponent<Field>();
+            if (field != null && _inventory.carrotSeed > 0)
+            {
                 field.TryToPlantAt(hitData.point);
-            }             
+                _inventory.carrotSeed--;
+                return;
+            }
         }
-        }
-        
-    }
+    }   
 }
 
